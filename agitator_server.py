5020e8de3b1c5b946b3d8c4fde3500b93d59d9d3
Ryan Petersburg <rpetersburg@gmail.com>
2017-12-06 15:33:31 -0500

    removing socket server

--- agitator_server.py
+++ agitator_server.py
@@ -1,25 +1,6 @@
-import socket
 from xmlrpc.server import SimpleXMLRPCServer
 import json
 from expres_agitator import Agitator
-
-class SocketServer:
-    host = None
-    port = None
-
-    def __init__(self, host, port):
-        self.set_host(host)
-        self.set_port(port)
-
-    def run(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((self.host, self.port))
-
-        while True: 
-            sock.listen(1)
-            conn, addr = sock.accept()
-            print('Connection from: {}'.format(addr))
-            serve_data(conn)
 
 class AgitatorRPCServer(SimpleXMLRPCServer):
 
