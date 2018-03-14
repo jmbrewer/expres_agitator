c729926fa829ab35212157a61893ce0859ac754c
Ryan Petersburg <rpetersburg@gmail.com>
2018-03-14 10:58:27 -0400

    better documentation

--- expres_agitator.py
+++ expres_agitator.py
@@ -1,9 +1,10 @@
 """
-    EXPRES Fiber Agitator Interface Class
+    EXPRES Fiber Agitator Interface Module
 
-    This provides a class to send commands to and receive information from
-    a dual-channel Roboclaw voltage controller controlling the two DC motors
-    for fiber agitation.
+    Provides a class to send commands to and receive information from a
+    dual-channel Roboclaw voltage controller controlling the two DC motors
+    for fiber agitation. Can also be run as a script to simply control the
+    fiber agitator from a terminal.
 """
 import numpy as np
 from time import sleep
@@ -18,6 +19,24 @@ __DEFAULT_RETRIES__ = 3
 __DEFAULT_INTER_BYTE_TIMEOUT__ = 1.0
 
 class Agitator(object):
+    '''Class for controlling the EXPRES fiber agitator
+    
+    Inputs
+    ------
+    comport : str
+        The hardware COM Port for the Roboclaw motor controller
+
+    Public Methods
+    --------------
+    start(exp_time, timeout, verbose, rot1, rot2):
+        Threaded agitation
+    start_agitation(exp_time, rot1, rot2, verbose):
+        Unthreaded agitation
+    stop(verbose):
+        Stop either threaded or unthreaded agitation
+    stop_agitation(verbose):
+        Hard-stop agitation but will not close thread
+    '''
 
     def __init__(self, comport=__DEFAULT_PORT__):
         self._rc = Roboclaw(comport=comport,
@@ -31,20 +50,24 @@ class Agitator(object):
         self.stop_agitation(verbose=False) # Just to make sure
 
     def __del__(self):
+        '''
+        When the object is deleted, make sure all threads are closed and
+        agitator is stopped.
+        '''
         print('Deleting Agitator object...')
-        self.stop()
+        self.stop(verbose=False)
         self.stop_agitation(verbose=False)
 
     def start(self, exp_time=60.0, timeout=600, verbose=True, **kwargs):
         '''
         Start a thread that starts agitation and stops if a stop event is
-        called or if a timeout is reached
+        called or if the timeout is reached
         '''
         self.stop(verbose) # To close any previously opened threads
 
         def threaded_agitation(exp_time, timeout, **kwargs):
             '''Nested function to allowing stop event'''
-            self.start_agitation(exp_time, **kwargs)
+            self.start_agitation(exp_time, verbose=verbose, **kwargs)
             t = 0
             while not self.stop_event.is_set() and t < timeout:
                 sleep(1)
@@ -52,7 +75,7 @@ class Agitator(object):
                 if verbose:
                     print('{}/{}s for {}s exposure'.format(t, timeout, exp_time))
                     print('I1: {}, I2: {}'.format(self.current1, self.current2))
-            self.stop_agitation()
+            self.stop_agitation(verbose)
             self.stop_event.clear() # Allow for future agitation events
 
         self.thread = Thread(target=threaded_agitation,
@@ -68,7 +91,8 @@ class Agitator(object):
                     print('Attempting to stop agitation...')
                 self.stop_event.set()
                 self.thread.join(2)
-        else: # As a backup in case something is going wrong
+        if self.voltage1 > 0 or self.voltage2 > 0:
+            # As a backup in case something went wrong
             self.stop_agitation(verbose)
 
     def start_agitation(self, exp_time=60.0, rot1=10.0, rot2=9.0, verbose=True):
@@ -91,6 +115,8 @@ class Agitator(object):
         '''Set both motor voltages to the given voltage'''
         self.set_voltage1(voltage)
         self.set_voltage2(voltage)
+
+    # Getters and setters for the motor voltages
 
     def get_voltage1(self):
         return self._voltage1
@@ -122,11 +148,6 @@ class Agitator(object):
 
     voltage2 = property(get_voltage2, set_voltage2)
 
-    def get_battery_voltage(self):
-        return self._rc.ReadMainBatteryVoltage()[1] / 10
-
-    battery_voltage = property(get_battery_voltage)
-
     def get_max_voltage(self):
         return self._rc.ReadMinMaxMainVoltages()[2] / 10
 
@@ -142,6 +163,15 @@ class Agitator(object):
         self._rc.SetMainVoltages(int(voltage*10), int(self.max_voltage*10))
 
     min_voltage = property(get_min_voltage, set_min_voltage)
+
+    # Getter for the motor controller power source voltage
+
+    def get_battery_voltage(self):
+        return self._rc.ReadMainBatteryVoltage()[1] / 10
+
+    battery_voltage = property(get_battery_voltage)
+
+    # Getters and setters for the motor currents
 
     def get_current1(self):
         return self._rc.ReadCurrents()[1]/100
@@ -170,10 +200,10 @@ class Agitator(object):
     max_current2 = property(get_max_current2, set_max_current2)
 
 class Motor:
-    """
+    '''
     Class that determines a voltage for a motor given the slope and intercept
     of the voltage vs. frequency regression
-    """
+    '''
     # Approximately the average parameters of Motor 1 and Motor 2
     slope = 28.0
     intercept = 1.8
@@ -181,9 +211,7 @@ class Motor:
     
     @classmethod
     def calc_voltage(cls, battery_voltage, exp_time=60.0, n_rotations=10.0):
-        """
-        Calculate the voltage for the motor given an exposure time in seconds
-        """
+        '''Calculate the voltage for the motor given an exposure time in seconds.'''
         if exp_time <= 0:
             exp_time = 2.0
         voltage = cls.slope / (exp_time/n_rotations) + cls.intercept
@@ -204,6 +232,19 @@ class Motor2(Motor):
     intercept = 1.58
 
 if __name__ == '__main__':
+    '''Script to allow terminal control of the motors
+    
+    To run, execute from the containing folder:
+    python expres_agitator.py <com_port>
+    where <com_port> is the name of the COM Port (typically COM13 on expres2)
+
+    Terminal will then show
+    Exposure time (s):
+
+    Input the appropriate exposure time and press enter. If you would like to
+    stop the agitator input 0. If you would like to close the program, input
+    anything that is not a number (or press ctrl-c).
+    '''
     import sys
 
     com_port = sys.argv[1]
